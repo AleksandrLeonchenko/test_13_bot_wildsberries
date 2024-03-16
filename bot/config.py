c9a31ae5ff6c9de4f12e1c2442c9a2fc9bd4a8d8
Aleksandr Leonchenko <kram.kab@mail.ru>
2024-03-16 13:44:38 +0300

    commit_8

--- bot/config.py
+++ bot/config.py
@@ -14,12 +14,25 @@ load_dotenv()
 # DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASS}@127.0.0.1/{POSTGRES_NAME}"
 
 
-POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
-POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
-POSTGRES_PASS = os.environ.get("POSTGRES_PASS")
-POSTGRES_USER = os.environ.get("POSTGRES_USER")
-POSTGRES_NAME = os.environ.get("POSTGRES_NAME")
+# POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+# POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
+# POSTGRES_PASS = os.environ.get("POSTGRES_PASS")
+# POSTGRES_USER = os.environ.get("POSTGRES_USER")
+# POSTGRES_NAME = os.environ.get("POSTGRES_NAME")
+#
+# BOT_TOKEN = os.environ.get("BOT_TOKEN")
+# DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASS}@postgres/{POSTGRES_NAME}"
+# # DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASS}@127.0.0.1/{POSTGRES_NAME}"
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+
+# POSTGRES_HOST = 127.0.0.1
+# POSTGRES_HOST = 'postgres'
+POSTGRES_PORT = 5432
+POSTGRES_PASS = '55660078aA'
+POSTGRES_USER = 'postgres'
+POSTGRES_NAME = 'test_13_wildsberries'
+
+BOT_TOKEN = '6335546830:AAFojQ0Jho4qTKXUnbz2AL57wfiHxPCIoBs'
 DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASS}@postgres/{POSTGRES_NAME}"
 # DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASS}@127.0.0.1/{POSTGRES_NAME}"
