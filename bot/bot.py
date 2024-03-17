91b8db18b012bf2d993e8ae9232f8ddd29176776
Aleksandr Leonchenko <kram.kab@mail.ru>
2024-03-17 10:18:39 +0300

    commit_11

--- bot/bot.py
+++ bot/bot.py
@@ -1,47 +1,9 @@
-# import asyncio
-# import logging
-# from aiogram import Bot, Dispatcher
-# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-# from sqlalchemy.orm import sessionmaker
-#
-# from config import BOT_TOKEN, DATABASE_URL
-# from data.database import Base, create_tables, main_2
-# from handlers import questions, different_types
-#
-# logging.basicConfig(level=logging.INFO)
-#
-#
-#
-#
-# async def main() -> None:
-#     """
-#     Асинхронная функция для запуска бота и начала обработки сообщений.
-#
-#     Raises:
-#         RuntimeError: Если произойдет ошибка при запуске бота.
-#     """
-#     bot = Bot(token=BOT_TOKEN)
-#     dp = Dispatcher()
-#
-#     dp.include_routers(questions.router, different_types.router)
-#
-#     await bot.delete_webhook(drop_pending_updates=True)
-#
-#     await dp.start_polling(bot, mylist=[1, 2, 3], mylist2=[11, 22, 33])
-#
-#
-# if __name__ == "__main__":
-#     # asyncio.run(init_db())
-#     asyncio.run(main())
-#     asyncio.run(main_2())
-
-
-
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+# import aiohttp
 
 from config import BOT_TOKEN, DATABASE_URL
 from data.database import Base, create_tables, main_2
@@ -68,5 +30,11 @@ async def main() -> None:
 
     await dp.start_polling(bot, mylist=[1, 2, 3], mylist2=[11, 22, 33])
 
+    # # Создаем сеанс и коннектор для aiohttp
+    # connector = aiohttp.TCPConnector()
+    # async with aiohttp.ClientSession(connector=connector) as session:
+    #     # Ваша логика работы с сессией
+
 if __name__ == "__main__":
     asyncio.run(main())
+
--- bot/config.py
+++ bot/config.py
@@ -3,36 +3,13 @@ import os
 
 load_dotenv()
 
-# POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-# POSTGRES_PORT = os.getenv("POSTGRES_PORT")
-# POSTGRES_PASS = os.getenv("POSTGRES_PASS")
-# POSTGRES_USER = os.getenv("POSTGRES_USER")
-# POSTGRES_NAME = os.getenv("POSTGRES_NAME")
-#
-# BOT_TOKEN = os.getenv("BOT_TOKEN")
-# # DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASS}@postgres/{POSTGRES_NAME}"
-# DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASS}@127.0.0.1/{POSTGRES_NAME}"
 
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
+POSTGRES_PASS = os.environ.get("POSTGRES_PASS")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_NAME = os.environ.get("POSTGRES_NAME")
 
-# POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
-# POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
-# POSTGRES_PASS = os.environ.get("POSTGRES_PASS")
-# POSTGRES_USER = os.environ.get("POSTGRES_USER")
-# POSTGRES_NAME = os.environ.get("POSTGRES_NAME")
-#
-# BOT_TOKEN = os.environ.get("BOT_TOKEN")
-# DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASS}@postgres/{POSTGRES_NAME}"
-# # DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASS}@127.0.0.1/{POSTGRES_NAME}"
-
-
-
-POSTGRES_HOST = '127.0.0.1'
-# POSTGRES_HOST = 'postgres'
-POSTGRES_PORT = 5432
-POSTGRES_PASS = '55660078aA'
-POSTGRES_USER = 'postgres'
-POSTGRES_NAME = 'test_13_wildsberries'
-
-BOT_TOKEN = '6335546830:AAFojQ0Jho4qTKXUnbz2AL57wfiHxPCIoBs'
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASS}@postgres/{POSTGRES_NAME}"
 # DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASS}@127.0.0.1/{POSTGRES_NAME}"
